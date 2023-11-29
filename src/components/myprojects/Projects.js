f6e0811a4e8fbb01b2197e237025020d66c88e6b
Pramod Chhetri <113252695+PramodChhetri@users.noreply.github.com>
2023-11-29 17:32:07 +0545

    projects added

--- src/components/myprojects/Projects.js
+++ src/components/myprojects/Projects.js
@@ -6,32 +6,53 @@ import FoodRhino from "../../assets/FoodRhino.png"
 import PortfolioPic from "../../assets/portfoliaposter.png";
 import LaravelAPI from "../../assets/laravelAPI.png";
 import LaravelAPISanctum from "../../assets/laravelAPISanctum.png";
+import NetflixClone from "../../assets/NetflixClone.png";
+import NewsWave from "../../assets/NewsWave.png";
+import AppointmentManager from "../../assets/AppointmentManager.png";
 
 
 const projectsData = [
   {
-    title: 'Evergreen',
+    title: 'Laravel',
     image: Evergreen,
     githubLink: 'https://github.com/PramodChhetri/Laravel-Evergreen',
-    liveDemoLink: '#'
+    liveDemoLink: 'https://evergreen.pramodchhetri.com.np'
   },
   {
-    title: 'TextNinja',
+    title: 'React',
     image: TextNinja,
     githubLink: 'https://github.com/PramodChhetri/React-TextNinja',
     liveDemoLink: 'https://pramodchhetri.github.io/React-TextNinja/'
   },
   {
-    title: 'FoodRhino',
+    title: 'Laravel',
+    image: AppointmentManager,
+    githubLink: 'https://github.com/PramodChhetri/Laravel-AMS',
+    liveDemoLink: 'https://ams.pramodchhetri.com.np'
+  },
+  {
+    title: 'React',
+    image: NetflixClone,
+    githubLink: 'https://github.com/PramodChhetri/React-NetflexClone',
+    liveDemoLink: 'https://pramodchhetri.github.io/React-NetflexClone/'
+  },
+  {
+    title: 'Core PHP',
     image: FoodRhino,
     githubLink: 'https://github.com/PramodChhetri/FoodRhino',
     liveDemoLink: '#'
   },
   {
+    title: 'React',
+    image: NewsWave,
+    githubLink: 'https://github.com/PramodChhetri/React-NewsWave',
+    liveDemoLink: 'https://pramodchhetri.github.io/React-NewsWave/'
+  },
+  {
     title: 'Portfolio',
     image: PortfolioPic,
     githubLink: 'https://github.com/PramodChhetri/Portfolia',
-    liveDemoLink: '#'
+    liveDemoLink: 'https://pramodchhetri.com.np'
   },
   {
     title: 'Laravel API',
